a207d7395780aefa80a5df455adfd455cbc1aa92
Vincent T <vyncode@gmail.com>
2021-03-29 14:26:08 -0400

    quiz features almost entirely done

--- app.js
+++ app.js
@@ -6,12 +6,24 @@ let quizBody = document.querySelector('.quizcard')
 let buttonWork = document.querySelector('#answercontainer')
 let nextButton = document.querySelector('#nextcard')
 let answerField = document.querySelector('#answercontainer')
+let wallCard = document.querySelector('#leaderwall')
 startButton.addEventListener('click', startQuiz)
+let retry = document.querySelector('#redoquiz')
+let clickLeader = document.querySelector('#leaderboardbutton')
+
 let randomQuestions
 let currentQuestion
 let nextCount = 0
+let playerPoints = 0
+let userLocation = 0
+
+// need username input
+// need point display
+// add to
 
 
+wallCard.classList.add('hide')
+clickLeader.addEventListener('click', leaderboard)
 
 if (currentQuestion >= 9) {
     nextButton.classList.add('hide')
@@ -41,6 +53,10 @@ function createQuestion(questionInput) {
     questionInput.answers.forEach(answer => {
         const button = document.createElement('button')
         button.innerText = answer.text
+        button.classList.add('available')
+        if(answer.correct) {
+            button.dataset.correct = answer.correct
+        }
         button.addEventListener('click',chooseAnswer)
         buttonWork.appendChild(button)
     });
@@ -52,33 +68,57 @@ function middleFunction() {
     nextCount++
     if (nextCount >= 10) {
         nextButton.classList.add('hide')
-        questionAsk.innerText = 'Quiz done'
+        questionAsk.innerText = `Quiz complete! USER earned ${playerPoints} / 10`
         // FUNCTION FOR LEADERBOARD RUN
+        leaderboard ()
     }
     nextButton.classList.add('hide')
     console.log(`answered ${nextCount}`)
     if (currentQuestion <= 9) {
-        resetQuestion()
+
         readyQuestion()}
 }
 
 function resetQuestion() {
 currentQuestion++;
+userLocation++
+questionAsk.innerText = `Quiz question ${userLocation}/10 complete! `
         while (buttonWork.firstChild) {
             buttonWork.removeChild
             (buttonWork.firstChild)
         }
 }
 
-function chooseAnswer() {
-    nextButton.classList.remove('hide')
-    nextButton.addEventListener('click', middleFunction)
+
+function removeChoice () {
+document.querySelectorAll('.available').classList.add('hide')
 }
 
+function chooseAnswer(e) {
+    const userChoice = e.target
+    const correctChoice = userChoice.dataset.correct
+    userChoice.classList.remove('available')
 
-//     // leaderboard launch func
 
-// }
+    nextButton.classList.remove('hide')
+    nextButton.addEventListener('click', middleFunction)
+    if (correctChoice) {
+        playerPoints++
+    } else if (!correctChoice) { console.log(` wrong answer. player points ${playerPoints}`) }
+    console.log(`player points ${playerPoints}`)
+    resetQuestion()
+
+}
+
+function reloadpage() {
+    location.reload()
+}
+
+function leaderboard () {
+    quizBody.classList.add('hide')
+    wallCard.classList.remove('hide')
+    retry.addEventListener('click', reloadpage)
+}
 
 const mainQuestionBank = [
     {question:"Inside which HTML element do we put the JavaScript?",
@@ -138,7 +178,7 @@ const mainQuestionBank = [
     answers: [
         { text: "while (i <= 1)", correct: true },
         { text: "while i = 1 to 10", correct: false },
-        { text: "while (i <= 1; i++)", correct: false },
+        { text: "while (i <= !)", correct: false },
         { text: "while loop = 1", correct: false }, ]
     },
 
